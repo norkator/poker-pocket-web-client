a9ef384c0c6f83cbaff5006fb7af1acfe6f63ccf
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-07-17 17:17:29 +0300

    remove port from production api url

--- web-client/config.js
+++ web-client/config.js
@@ -1,6 +1,6 @@
 // WebSocket server URL
 const API_URL_DEVELOPMENT = "ws://localhost:8000";
-const API_URL_PRODUCTION = "wss://pokerpocket.nitramite.com:8001";
+const API_URL_PRODUCTION = "wss://pokerpocket.nitramite.com";
 
 // UI settings
 const LS_MODE_TOGGLE_STATE = "LS_MODE_TOGGLE_STATE";
